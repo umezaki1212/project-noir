e9b984a7a0749781622c721b23d8cba41de40cb9
umezaki1212 <umezaki1212@gmail.com>
2021-10-03 01:59:42 +0900

    Initial commit

---
+++ readme.md
@@ -0,0 +1,5 @@
+### Generate SSL
+* https://github.com/FiloSottile/mkcert
+```
+mkcert -cert-file ./docker/certs/default.crt -key-file ./docker/certs/default.key local-dev.noir.ai
+```
---
+++ src/laravel/routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::group(['prefix' => 'api'], function () {
+    Route::get('/', function () {
+        return view('welcome');
+    });
+    Route::get('/version', function () {
+        return response()->json([
+            'Version' => app()->version()
+        ]);
+    });
+});
